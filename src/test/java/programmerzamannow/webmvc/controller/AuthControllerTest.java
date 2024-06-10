7e3d72e3386a9e3cae1aac0ca2f2ef40234d4400
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2024-06-10 11:14:08 +0700

    menambah cara menggunakan response status

---
+++ src/test/java/programmerzamannow/webmvc/controller/AuthControllerTest.java
@@ -0,0 +1,49 @@
+package programmerzamannow.webmvc.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AuthControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void loginSuccess() throws Exception {
+        mockMvc.perform(
+                post("/auth/login")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("username", "eko")
+                        .param("password", "rahasia")
+        ).andExpectAll(
+                status().isOk(),
+                content().string(Matchers.containsString("OK"))
+        );
+    }
+
+    @Test
+    void loginFailes() throws Exception {
+        mockMvc.perform(
+                post("/auth/login")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("username", "salah")
+                        .param("password", "rahasia")
+        ).andExpectAll(
+                status().isUnauthorized(),
+                content().string(Matchers.containsString("KO"))
+        );
+    }
+}
